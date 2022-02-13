8a6da5dff6644bb385d193d968e1b632acb6a0d6
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-02-13 19:59:01 +0200

    Initial Commit

---
+++ WordleInSwiftUI/WordleInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleInSwiftUIApp.swift
+//  WordleInSwiftUI
+//
+//  Created by ramil on 13.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordleInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
